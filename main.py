a935e551a26361ec18a950021ca698e934a8b67f
bjavohir2197 <bjavohir2197@gmail.com>
2026-01-05 12:56:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import matn_untils
+
+text = "Salom Python"
+
+res1 = matn_untils.katta_harf(text)
+print(res1)
+
+res2 = matn_untils.teskari(text)
+print(res2)
+
+res3 = matn_untils.sozlar_soni(text)
+print(res3)
